6abf56a4adfeca13b81989f74c6f153ffe6f7355
ziliwalid <walidzili19@gmail.com>
2024-01-21 12:34:30 -0800

    commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# makes some changes int config file
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
