56dbd315ca0a850dadf7421bd3379fab5150f303
ziliwalid <walidzili19@gmail.com>
2024-03-06 02:16:29 -0800

    commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+gets the subs from a sub redit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """gets sub numner from  a sub reddit"""
+    uri = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    res = requests.get(uri, headers=headers, allow_redirects=False)
+    """code 200 means ok"""
+    if res.status_code == 200:
+        data = res.json()
+        subs = data['data']['subscribers']
+        return subs
+    else:
+        return 0
