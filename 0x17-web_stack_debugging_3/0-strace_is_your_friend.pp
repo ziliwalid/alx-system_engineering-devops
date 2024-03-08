298d71b07359209d98bf354dc1f2e7418717becb
ziliwalid <walidzili19@gmail.com>
2024-03-08 07:29:57 -0800

    commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,8 @@
 
 $filePath = '/var/www/html/wp-settings.php'
 
+# turnes `phpp` extensions to `php` in the file `wp-settings.php`
+
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${filePath}",
   path    => ['/bin','/usr/bin']
