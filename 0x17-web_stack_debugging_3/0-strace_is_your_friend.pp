32b0ac7c63faa5a175dc4727b42255cae4ec5dd4
Walid <83774096+ziliwalid@users.noreply.github.com>
2024-03-08 16:34:26 +0100

    Update 0-strace_is_your_friend.pp
    fixes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# turnes (phpp) extensions to (php) in the given file
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
